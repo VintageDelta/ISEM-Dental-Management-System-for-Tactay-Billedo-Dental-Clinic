9b2fdde01c29da0aab227892cd4a8073f970c7a6
renuAd <rhino.adame@gmail.com>
2025-09-20 15:11:38 +0800

    Added Profile View and Log Out

--- isem/userprofile/urls.py
+++ isem/userprofile/urls.py
@@ -7,4 +7,6 @@ app_name = 'userprofile'
 urlpatterns = [
     path('sign-in/', views.signin, name='signin'),
     path('sign-up/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
--- isem/userprofile/views.py
+++ isem/userprofile/views.py
@@ -22,7 +22,8 @@ def signin(request):
 
 def signup(request):
     if request.method == "POST":
-        full_name = request.POST.get("full_name")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
         email = request.POST.get("email")
         username = request.POST.get("username")
         password1 = request.POST.get("password1")
@@ -48,9 +49,19 @@ def signup(request):
         
         #user_creation
         user = User.objects.create_user(username=username, email=email, password=password1)
-        user.first_name = full_name
+        user.first_name = first_name
+        user.last_name = last_name
         user.save()
         messages.success(request, "Account created successfully. Please sign in.")
         return redirect("userprofile:signin")
     
     return render(request, 'userprofile/sign-up.html')
+
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect("userprofile:signin")
+    return render(request, 'userprofile/profile.html')
+
+def logout(request):
+    messages.success(request, "You have been logged out.")
+    return redirect("userprofile:signin")
